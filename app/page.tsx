da8c889c975fbbe0e774437623039160d8bf7938
ARZIELLE ARCEGA <jodespanola@gmail.com>
2023-07-23 00:17:45 +0800

    Fourth Commit
    Changed API fetch request to useSWRInfinite to be able to do multiple
    calls

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import UserCard from "./components/UserCard";
 import { useState, useEffect } from 'react'
-import useSWR from 'swr'
+import useSWRInfinite from 'swr/infinite'
 
 type user = {
   id: number,
@@ -12,30 +12,35 @@ type user = {
   avatar: string,
 }
 
-// const getData = async (page: number) => {
-
-// }
-
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-export default function Home() {
+const getKey = (pageIndex: any, previousPageData: any) => {
+  if (previousPageData && !previousPageData.total_pages) return null // reached the end
+  return `https://reqres.in/api/users?page=${pageIndex+1}&limit=10`                    // SWR key
+}
 
-  const {data, error, isLoading} = useSWR("https://reqres.in/api/users?page=1", fetcher) 
-  if (error) return <h1>Failed to connect to API</h1>
-  if (isLoading) return <h1>Page is Loading</h1>
+export default function Home() {
+  const [numUsers, setNumUsers] = useState(12)
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher)
+
+  if (!data || data===undefined) return <h1>Failed to connect to API</h1>
   
   return (
     <main className='flex flex-col justify-center items-center 
       min-h-screen gap-5'>
       <div className='bg-none w-screen grid justify-center items-center
       gap-5 py-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:px-20'>
-        {data.data.map((user: user) => {
+        {/* data is an array of each api request */}
+        {data.map((page) => page.data.map((user: user) => {
           return (<UserCard user_id={user.id} email={user.email} 
             first={user.first_name} last={user.last_name} avatar={user.avatar}/>)
-        })}
+        }))}
       </div>
-      <button className='bg-gradient-to-tl from-dark_blue to-light_blue
-        px-5 py-3 rounded-2xl text-white shadow-lg'>
+      <button className={`${size+1 === data[0].total_pages ? '' : 'opacity-50'}
+        bg-gradient-to-tl from-dark_blue to-light_blue
+        px-5 py-3 rounded-2xl text-white shadow-lg`}
+        disabled={size+1 === data[0].total_pages ? false : true}
+        onClick={() => setSize(size + 1)}>
         Load More
       </button>
     </main>
